cba3fe9b9260b389a3c3fc35dfe37281b0373220
Darko Kezic <dkezic@yahoo.com>
2018-09-21 21:54:13 +0200

    Add https support

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # pure-node-api-app
+
+How to generate cert and pem files in https folder:
+
+openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout key.pem -out cert.pem
--- index.js
+++ index.js
@@ -5,13 +5,35 @@
 
 // Dependencies
 var http = require('http');
+var https = require('https');
 var url = require('url');
 var StringDecoder = require('string_decoder').StringDecoder;
 var config = require('./config');
+var fs = require('fs');
 
- // Configure the server to respond to all requests with a string
-var server = http.createServer(function(req,res){
+var httpServer = http.createServer(function(req,res){
+  unifiedServer(req,res);
+});
 
+// Start the server
+httpServer.listen(config.httpPort,function(){
+  console.log('The server is up and running on port '+config.httpPort);
+});
+
+var httpsServerOptions = {
+  'key' : fs.readFileSync('./https/key.pem'),
+  'cert' : fs.readFileSync('./https/cert.pem')
+};
+var httpsServer = https.createServer(httpsServerOptions, function(req,res){
+  unifiedServer(req,res);
+});
+
+// Start the server
+httpsServer.listen(config.httpsPort,function(){
+  console.log('The server is up and running on port '+config.httpsPort);
+});
+
+var unifiedServer = function(req, res) {
   // Parse the url
   var parsedUrl = url.parse(req.url, true);
 
@@ -70,12 +92,7 @@ var server = http.createServer(function(req,res){
       });
 
   });
-});
-
-// Start the server
-server.listen(config.port,function(){
-  console.log('The server is up and running on port '+config.port+' in '+config.envName+' mode.');
-});
+};
 
 // Define all the handlers
 var handlers = {};
